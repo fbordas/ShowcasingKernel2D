6a8487cb9fcb964255c09bf15ccecbf5a66cd154
TohnoCoding <fbordas+tohnocoding@gmail.com>
2025-05-21 16:23:33 -0400

    Changes

---
+++ EmptyProject/EmptyProject.Core/BaseLogicComponents/Animation/Spritesheet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace EmptyProject.Core.BaseLogicComponents.Animation
+{
+    internal class Spritesheet
+    {
+        public Texture2D Texture { get; set; }
+        public string Name { get; set; }
+        public Dictionary<string, SpriteAnimation> Animations;
+    }
+}
